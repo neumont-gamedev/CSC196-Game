7b077ecbd9f215b98e14535500d10c91d116fca2
Raymond L Maple <rmaple@neumont.edu>
2023-06-29 15:14:13 -0600

    class 062923

--- Source/Engine/Core/Memory.cpp
+++ Source/Engine/Core/Memory.cpp
@@ -2,17 +2,40 @@
 #include <iostream>
 
 using namespace std;
+kiko::MemoryTracker kiko::g_memoryTracker;
 
 void* operator new (size_t size)
 {
-	cout << "allocated: " << size << endl;
+	void* p = malloc(size);
 
-	return malloc(size);
+	kiko::g_memoryTracker.Add(p, size);
+
+	return p;
 }
 
 void operator delete (void* address, size_t size)
 {
-	cout << "deallocated: " << size << endl;
-
+	kiko::g_memoryTracker.Remove(address, size);
 	free(address);
+}
+
+namespace kiko
+{
+	void MemoryTracker::Add(void* address, size_t size)
+	{
+		m_bytesAllocated += size;
+		m_numAllocations++;
+	}
+
+	void MemoryTracker::Remove(void* address, size_t size)
+	{
+		m_bytesAllocated -= size;
+		m_numAllocations--;
+	}
+
+	void MemoryTracker::DisplayInfo()
+	{
+		cout << "current bytes allocated: " << m_bytesAllocated << endl;
+		cout << "current number allocations: " << m_numAllocations << endl;
+	}
 }
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,30 +1,48 @@
 #include "Core/Random.h"
 #include "Core/FileIO.h"
 #include "Core/Memory.h"
+#include "Core/Time.h"
+#include "Renderer/Renderer.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	kiko::g_memoryTracker.DisplayInfo();
 	int* p = new int;
+	kiko::g_memoryTracker.DisplayInfo();
 	delete p;
+	kiko::g_memoryTracker.DisplayInfo();
 
-	cout << kiko::getFilePath() << endl;
-	kiko::setFilePath("Assets");
-	cout << kiko::getFilePath() << endl;
-	size_t size = 0;
-	kiko::getFileSize("game.txt", size);
-	cout << size << endl;
+	kiko::Time timer;
+	for (int i = 0; i < 1000000; i++) {}
+	cout << timer.GetElapsedSeconds() << endl;
 
-	std::string s;
-	kiko::readFile("game.txt", s);
-	cout << s << endl;
+	//auto start = std::chrono::high_resolution_clock::now();
+	//for (int i = 0; i < 1000000; i++) {}
+	//auto end = std::chrono::high_resolution_clock::now();
+
+	//cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << endl;
 
 
-	kiko::seedRandom((unsigned int)time(nullptr));
-	for (int i = 0; i < 3; i++)
-	{
-		cout << kiko::random(5, 10) << endl;
-	}
+
+
+	//cout << kiko::getFilePath() << endl;
+	//kiko::setFilePath("Assets");
+	//cout << kiko::getFilePath() << endl;
+	//size_t size = 0;
+	//kiko::getFileSize("game.txt", size);
+	//cout << size << endl;
+
+	//std::string s;
+	//kiko::readFile("game.txt", s);
+	//cout << s << endl;
+
+
+	//kiko::seedRandom((unsigned int)time(nullptr));
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	cout << kiko::random(5, 10) << endl;
+	//}
 }
