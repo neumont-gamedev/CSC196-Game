4f2b48232cf6ab0404c4d7c0a256a7be3fcc667a
Raymond L Maple <rmaple@neumont.edu>
2023-07-05 16:04:14 -0600

    class 070523

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -9,6 +9,24 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	kiko::CreateWindow("CSC196", 800, 600);
-	cin.get(); // pause
+	kiko::Renderer renderer;
+	renderer.Initialize();
+	renderer.CreateWindow("CSC196", 800, 600);
+
+	while (true)
+	{
+		renderer.SetColor(0, 0, 0, 0);
+		renderer.BeginFrame();
+		// draw
+		for (int i = 0; i < 10000; i++)
+		{
+			renderer.SetColor(kiko::random(256), kiko::random(256), kiko::random(256), 255);
+			renderer.DrawPoint(kiko::random(renderer.GetWidth()), kiko::random(renderer.GetHeight()));
+		}
+		
+		
+		renderer.EndFrame();
+	}
+
+	return 0;
 }
