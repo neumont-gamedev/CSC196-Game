0f89ef76629024282e225a3b5bebcca14acca801
Raymond L Maple <rmaple@neumont.edu>
2023-07-30 17:29:02 -0600

    project complete

--- Source/Engine/Framework/Actor.h
+++ Source/Engine/Framework/Actor.h
@@ -20,7 +20,7 @@ namespace kiko
 		virtual void Update(float dt);
 		virtual void Draw(kiko::Renderer& renderer);
 
-		float GetRadius() { return (m_model) ? m_model->GetRadius() * m_transform.scale : 0; }
+		float GetRadius() { return (m_model) ? m_model->GetRadius() * m_transform.scale : -10000; }
 		virtual void OnCollision(Actor* other) {}
 
 		void AddForce(const vec2& force) { m_velocity += force; }
